8418e8b84f5257fdc1b8413ce07eda310fbbf487
Leonardo <leonardo.martinezv@softtek.com>
2021-01-15 19:53:38 -0600

    Custom commercewebservices extension

---
+++ core-customize/hybris/bin/custom/customcommercews/src/com/lmv/queues/impl/OrderStatusUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.lmv.queues.impl;
+
+import com.lmv.queues.data.OrderStatusUpdateElementData;
+
+
+/**
+ * Queue for {@link com.lmv.queues.data.OrderStatusUpdateElementData}
+ */
+public class OrderStatusUpdateQueue extends AbstractUpdateQueue<OrderStatusUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
---
+++ core-customize/hybris/bin/custom/customcommercews/src/com/lmv/queues/impl/ProductExpressUpdateQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.lmv.queues.impl;
+
+import com.lmv.queues.data.ProductExpressUpdateElementData;
+
+
+/**
+ * Queue for {@link com.lmv.queues.data.ProductExpressUpdateElementData}
+ */
+public class ProductExpressUpdateQueue extends AbstractUpdateQueue<ProductExpressUpdateElementData>
+{
+	// EMPTY - just to instantiate bean
+}
